e0f12b9da7123848d833f7fbbc1d092e6fda3934
Tao Xiang <xiang.tao@outlook.de>
2022-12-07 21:24:32 +0100

    1

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="text2music",
+    version="0.0.1",
+    author="NLP Guild",
+    author_email="tao.xiang@tum.de",
+    description="A package for text-to-music generation",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NLP-Guild/text2music",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+  "Programming Language :: Python :: 3.8",
+  "License :: OSI Approved :: MIT License",
+    ],
+)
