c39b00e2c0c98b304d3bef511a75a4b6834f4607
Tao Xiang <46348901+leoxiang66@users.noreply.github.com>
2022-12-08 14:18:15 +0000

    updates

--- text2music/experimental/__init__.py
+++ text2music/experimental/__init__.py
@@ -1,1 +1,1 @@
-from .mubert__ import generate_track_by_prompt as mubert_generate
+from .mubert__ import generate_and_store as mubert_generate
--- text2music/experimental/mubert__.py
+++ text2music/experimental/mubert__.py
@@ -4,6 +4,7 @@ import numpy as np
 import httpx
 import time
 from sentence_transformers import SentenceTransformer
+import requests
 
 MUBERT_TAGS_STRING = 'tribal,action,kids,neo-classic,run 130,pumped,jazz / funk,ethnic,dubtechno,reggae,acid jazz,liquidfunk,funk,witch house,tech house,underground,artists,mystical,disco,sensorium,r&b,agender,psychedelic trance / psytrance,peaceful,run 140,piano,run 160,setting,meditation,christmas,ambient,horror,cinematic,electro house,idm,bass,minimal,underscore,drums,glitchy,beautiful,technology,tribal house,country pop,jazz & funk,documentary,space,classical,valentines,chillstep,experimental,trap,new jack swing,drama,post-rock,tense,corporate,neutral,happy,analog,funky,spiritual,sberzvuk special,chill hop,dramatic,catchy,holidays,fitness 90,optimistic,orchestra,acid techno,energizing,romantic,minimal house,breaks,hyper pop,warm up,dreamy,dark,urban,microfunk,dub,nu disco,vogue,keys,hardcore,aggressive,indie,electro funk,beauty,relaxing,trance,pop,hiphop,soft,acoustic,chillrave / ethno-house,deep techno,angry,dance,fun,dubstep,tropical,latin pop,heroic,world music,inspirational,uplifting,atmosphere,art,epic,advertising,chillout,scary,spooky,slow ballad,saxophone,summer,erotic,jazzy,energy 100,kara mar,xmas,atmospheric,indie pop,hip-hop,yoga,reggaeton,lounge,travel,running,folk,chillrave & ethno-house,detective,darkambient,chill,fantasy,minimal techno,special,night,tropical house,downtempo,lullaby,meditative,upbeat,glitch hop,fitness,neurofunk,sexual,indie rock,future pop,jazz,cyberpunk,melancholic,happy hardcore,family / kids,synths,electric guitar,comedy,psychedelic trance & psytrance,edm,psychedelic rock,calm,zen,bells,podcast,melodic house,ethnic percussion,nature,heavy,bassline,indie dance,techno,drumnbass,synth pop,vaporwave,sad,8-bit,chillgressive,deep,orchestral,futuristic,hardtechno,nostalgic,big room,sci-fi,tutorial,joyful,pads,minimal 170,drill,ethnic 108,amusing,sleepy ambient,psychill,italo disco,lofi,house,acoustic guitar,bassline house,rock,k-pop,synthwave,deep house,electronica,gabber,nightlife,sport & fitness,road trip,celebration,electro,disco house,electronic'
 MUBERT_TAGS = np.array(MUBERT_TAGS_STRING.split(','))
@@ -104,5 +105,9 @@ def generate_track_by_prompt(prompt, duration, loop=False):
         return None, str(e), ""
 
 
-def generate_and_store(prompt,duration,loop=False):
-    url, state, 
+def generate_and_store(prompt,duration,output_path: str,loop=False,):
+    url, state, tags = generate_track_by_prompt(prompt,duration,loop)
+    r = requests.get(url, allow_redirects=True)
+
+    open(f'{output_path}.mp3', 'wb').write(r.content)
+
