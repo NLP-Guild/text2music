6234845e77d94ca06cbc9a46ae05ae7c8a86721a
Tao Xiang <xiang.tao@outlook.de>
2022-12-09 12:47:51 +0100

    1

---
+++ assets/js/myjs.js
@@ -0,0 +1,9 @@
+
+function show_sidebar() {
+    document.getElementById("mySidebar").style.block = "visible";
+
+}
+
+function hide_sidebar() {
+    document.getElementById("mySidebar").style.visibility = 'hidden';
+}
