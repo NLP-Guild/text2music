d0e2c239f54ff38e66af192fe5019801ace82570
Tao Xiang <tao.xiang@tum.de>
2022-12-10 10:13:39 +0100

    Revert "updates"
    This reverts commit 77917bc1ea8b0f16f2509288cedc644545448af0.

--- assets/js/9ffe0064.7dccd919.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocusaurus=self.webpackChunkdocusaurus||[]).push([[455],{5745:u=>{u.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
